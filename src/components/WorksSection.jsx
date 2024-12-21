a89fdbba9b181be79dc727cdea9626368baf5070
Dae-de-bug <dioncedrickmarquez@gmail.com>
2024-12-21 15:18:22 +0800

    Updated styles and layout for responsiveness

--- src/components/WorksSection.jsx
+++ src/components/WorksSection.jsx
@@ -1,112 +1,121 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
-import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';  // Importing left and right chevron icons from react-icons
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import './WorksSection.css';
 
-// Define the activities with files (images only)
+// Photography images
+import photo1 from '../assets/images/photography/photo_6161444282389611543_y.jpg';
+import photo2 from '../assets/images/photography/photo_6161444282389611557_y.jpg';
+import photo3 from '../assets/images/photography/photo_6161444282389611573_y.jpg';
+import photo4 from '../assets/images/photography/photo_6161444282389611576_y.jpg';
+import photo5 from '../assets/images/photography/photo_6161444282389611577_y.jpg';
+import photo6 from '../assets/images/photography/photo_6161444282389611579_y.jpg';
+import photo7 from '../assets/images/photography/photo_6161444282389611582_y.jpg';
+import photo8 from '../assets/images/photography/photo_6161444282389611593_y.jpg';
+
+// Graphic Design images
+import design1 from '../assets/images/graphic-design/activity 5(1).jpg';
+import design2 from '../assets/images/graphic-design/activity 5(2).jpg';
+import design3 from '../assets/images/graphic-design/activity 5(3).jpg';
+import design4 from '../assets/images/graphic-design/activity 5(4).jpg';
+import design5 from '../assets/images/graphic-design/activity 5(5).jpg';
+import design6 from '../assets/images/graphic-design/activity 5(6).jpg';
+import design7 from '../assets/images/graphic-design/activity 5(7).jpg';
+
+// Layout images
+import layout1 from '../assets/images/layout/Marquez-reference.jpg';
+import layout2 from '../assets/images/layout/Marquez.jpg';
+
+// Logo Design images
+import logo1 from '../assets/images/logo-design/cococrafts.jpg';
+import logo2 from '../assets/images/logo-design/1.png';
+import logo3 from '../assets/images/logo-design/2.png';
+import logo4 from '../assets/images/logo-design/3.png';
+import logo5 from '../assets/images/logo-design/4.png';
+
+// Visual Branding images
+import branding1 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-0.jpg';
+import branding2 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-1.jpg';
+import branding3 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-2.jpg';
+import branding4 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-3.jpg';
+import branding5 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-4.jpg';
+import branding6 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-5.jpg';
+import branding7 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-6.jpg';
+import branding8 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-7.jpg';
+import branding9 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-8.jpg';
+import branding10 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-9.jpg';
+import branding11 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-10.jpg';
+import branding12 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-11.jpg';
+import branding13 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-12.jpg';
+import branding14 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-13.jpg';
+import branding15 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-14.jpg';
+import branding16 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-15.jpg';
+import branding17 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-16.jpg';
+import branding18 from '../assets/images/visual-branding/G1-IT-145-Midterm-Project-images-17.jpg';
+
+// Merch-Packaging Design images
+import merch1 from '../assets/images/merch-packaging-design/1.png';
+import merch2 from '../assets/images/merch-packaging-design/2.png';
+import merch3 from '../assets/images/merch-packaging-design/3.png';
+import merch4 from '../assets/images/merch-packaging-design/4.png';
+import merch5 from '../assets/images/merch-packaging-design/5.png';
+
+
+// Website and Social Media Design images
+import web1 from '../assets/images/website-and-social-media-design/websocial1.png';
+import web2 from '../assets/images/website-and-social-media-design/websocial2.png';
+import web3 from '../assets/images/website-and-social-media-design/websocial3.png';
+import web4 from '../assets/images/website-and-social-media-design/websocial4.png';
+import web5 from '../assets/images/website-and-social-media-design/websocial6.png';
+import web6 from '../assets/images/website-and-social-media-design/websocial6.png';
+import web7 from '../assets/images/website-and-social-media-design/websocial7.png';
+
+
 const worksData = [
   {
     title: 'Photography',
     description: 'A collection of my photography work, capturing moments and landscapes.',
     datePosted: 'November 2024',
-    files: [
-      { type: 'image', name: 'photo_6161444282389611543_y.jpg' },
-      { type: 'image', name: 'photo_6161444282389611557_y.jpg' },
-      { type: 'image', name: 'photo_6161444282389611573_y.jpg' },
-      { type: 'image', name: 'photo_6161444282389611576_y.jpg' },
-      { type: 'image', name: 'photo_6161444282389611577_y.jpg' },
-      { type: 'image', name: 'photo_6161444282389611579_y.jpg' },
-      { type: 'image', name: 'photo_6161444282389611582_y.jpg' },
-      { type: 'image', name: 'photo_6161444282389611593_y.jpg' },
-    ]
+    files: [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8],
   },
   {
     title: 'Graphic Design',
     description: 'Designing visually appealing graphics for brands and businesses.',
     datePosted: 'February 2024',
-    files: [
-      { type: 'image', name: 'activity 5(1).jpg' },
-      { type: 'image', name: 'activity 5(2).jpg' },
-      { type: 'image', name: 'activity 5(3).jpg' },
-      { type: 'image', name: 'activity 5(4).jpg' },
-      { type: 'image', name: 'activity 5(5).jpg' },
-      { type: 'image', name: 'activity 5(6).jpg' },
-      { type: 'image', name: 'activity 5(7).jpg' }
-    ]
+    files: [design1, design2, design3, design4, design5, design6, design7],
   },
   {
     title: 'Layout',
     description: 'Creating layouts for magazines, books, and other printed materials.',
     datePosted: 'March 2024',
-    files: [
-      { type: 'image', name: 'Marquez reference.jpg' },
-      { type: 'image', name: 'Marquez.jpg' }
-    ]
+    files: [layout1, layout2],
   },
   {
     title: 'Logo Design',
     description: 'Designing unique and memorable logos for brands and businesses.',
     datePosted: 'April 2024',
-    files: [
-      { type: 'image', name: 'cococrafts.jpg' },
-      { type: 'image', name: 'Screenshot 2024-12-21 115142.png' },
-      { type: 'image', name: 'Screenshot 2024-12-21 115146.png' },
-      { type: 'image', name: 'Screenshot 2024-12-21 115151.png' },
-      { type: 'image', name: 'Screenshot 2024-12-21 115156.png' }
-    ]
+    files: [logo1, logo2, logo3, logo4, logo5],
   },
   {
     title: 'Visual Branding',
     description: 'Developing cohesive visual identities for businesses and brands.',
     datePosted: 'May 2024',
-    files: [
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-0.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-1.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-2.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-3.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-4.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-5.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-6.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-7.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-8.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-9.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-10.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-11.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-12.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-13.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-14.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-15.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-16.jpg' },
-      { type: 'image', name: 'G1-IT-145-Midterm-Project-images-17.jpg' }
-
-    ]
+    files: [branding1, branding2, branding3, branding4, branding5, branding6, branding7, branding8, branding9, branding10
+      , branding11, branding12, branding13, branding14, branding15, branding16, branding17, branding18
+    ], 
   },
   {
     title: 'Merch-Packaging Design',
     description: 'Designing packaging and merchandise for products and brands.',
     datePosted: 'June 2024',
-    files: [
-      { type: 'image', name: '1.png' },
-      { type: 'image', name: '2.png' },
-      { type: 'image', name: '3.png' },
-      { type: 'image', name: '4.png' },
-      { type: 'image', name: '5.png' }
-    ]
+    files: [merch1, merch2, merch3, merch4, merch5], // Add more imports as needed
   },
   {
     title: 'Website and Social Media Design',
     description: 'Designing websites and social media layouts to engage users.',
     datePosted: 'July 2024',
-    files: [
-      { type: 'image', name: 'websocial1.png' },
-      { type: 'image', name: 'websocial2.png' },
-      { type: 'image', name: 'websocial3.png' },
-      { type: 'image', name: 'websocial4.png' },
-      { type: 'image', name: 'websocial5.png' },
-      { type: 'image', name: 'websocial6.png' },
-      { type: 'image', name: 'websocial7.png' }
-    ]
-  }
+    files: [web1, web2, web3, web4, web5, web6, web7], // Add more imports as needed
+  },
 ];
 
 const WorksSection = () => {
@@ -115,7 +124,7 @@ const WorksSection = () => {
 
   const handleActivityClick = (activity) => {
     setSelectedActivity(activity);
-    setCurrentImageIndex(0); // Reset image index when a new activity is selected
+    setCurrentImageIndex(0);
   };
 
   const handleCloseModal = () => {
@@ -134,23 +143,15 @@ const WorksSection = () => {
     }
   };
 
-  const renderImageContent = (file) => {
-    const filePath = `/assets/images/${selectedActivity.title.toLowerCase().replace(/\s+/g, '-')}/${file.name}`;
-    return <img src={filePath} alt={file.name} className="modal-image" />;
+  const downloadImage = (url) => {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = url.split('/').pop();
+    link.click();
   };
 
-  const handleDownload = (fileName) => {
-    const filePath = `/assets/images/${selectedActivity.title.toLowerCase().replace(/\s+/g, '-')}/${fileName}`;
-    const a = document.createElement('a');
-    a.href = filePath;
-    a.download = fileName;
-    a.click();
-  };
-
-  const handleDownloadAll = () => {
-    selectedActivity.files.forEach((file) => {
-      handleDownload(file.name);
-    });
+  const downloadAll = () => {
+    selectedActivity.files.forEach((file) => downloadImage(file));
   };
 
   return (
@@ -170,7 +171,7 @@ const WorksSection = () => {
         ))}
       </div>
 
-      {/* Modal to show details */}
+      {/* Modal */}
       <Modal
         isOpen={selectedActivity !== null}
         onRequestClose={handleCloseModal}
@@ -178,34 +179,38 @@ const WorksSection = () => {
         className="modal-content"
         overlayClassName="modal-overlay"
       >
-        <button className="close-btn" onClick={handleCloseModal}>X</button>
         {selectedActivity && (
           <>
+            <button className="close-btn" onClick={handleCloseModal}>X</button>
             <h2>{selectedActivity.title}</h2>
             <p>{selectedActivity.description}</p>
-            <p><strong>Date Posted:</strong> {selectedActivity.datePosted}</p>
-            <div className="modal-files">
-              {selectedActivity.files.length > 1 && (
-                <div className="image-navigation">
-                  <button onClick={handlePrevImage} className="nav-btn">
-                    <FiChevronLeft />
-                  </button>
-                  <button onClick={handleNextImage} className="nav-btn">
-                    <FiChevronRight />
-                  </button>
-                </div>
-              )}
-              <div className="file-container">
-                {renderImageContent(selectedActivity.files[currentImageIndex])}
+            <img
+              src={selectedActivity.files[currentImageIndex]}
+              alt={`${selectedActivity.title} ${currentImageIndex + 1}`}
+              className="modal-image"
+            />
+            {selectedActivity.files.length > 1 && (
+              <div className="image-navigation">
+                <button className="nav-btn" onClick={handlePrevImage}>
+                  <FiChevronLeft />
+                </button>
+                <button className="nav-btn" onClick={handleNextImage}>
+                  <FiChevronRight />
+                </button>
               </div>
-              <div className="download-options">
-                <button className="download-btn" onClick={() => handleDownload(selectedActivity.files[currentImageIndex].name)}>Download This Image</button>
-                {selectedActivity.files.length > 1 && (
-                  <button className="download-all-btn" onClick={handleDownloadAll}>Download All Images</button>
-                )}
-              </div>
+            )}
+            {/* Download Buttons */}
+            <div className="download-options">
+              <button 
+                className="download-btn" 
+                onClick={() => downloadImage(selectedActivity.files[currentImageIndex])}
+              >
+                Download This Image
+              </button>
+              <button className="download-all-btn" onClick={downloadAll}>
+                Download All
+              </button>
             </div>
-            {/* <button className="close-modal-btn" onClick={handleCloseModal}>Close</button> */}
           </>
         )}
       </Modal>
